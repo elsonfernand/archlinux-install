3fc715b4f397c40570d3fb6a91b7a72b95b34ef4
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-02 17:52:24 -0300

    Update kde-minimal-el.sh

--- kde-minimal-el.sh
+++ kde-minimal-el.sh
@@ -5,14 +5,18 @@ sudo hwclock --systohc
 
 ## Reflector
 # Instalando
-pacman -S --noconfirm reflector
+sudo pacman -S --noconfirm reflector
 # Ativando
 systemctl enable reflector.timer
 # Configurando
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup ; reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Instalação do KDE Plasma 6 sem Discover e do gerenciador de login
-sudo pacman -S --noconfirm bash-completion fastfetch plasma-desktop git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber nvidia nvidia-utils nvidia-settings dolphin dolphin-plugins ffmpegthumbs ark konsole okular gwenview kscreen chromium kate kde-gtk-config kcalc sddm sddm-kcm --ignore discover
+sudo pacman -S --noconfirm bash-completion fastfetch plasma-desktop git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber nvidia nvidia-utils nvidia-settings dolphin dolphin-plugins ffmpegthumbs tlp ark konsole okular gwenview kscreen chromium kate kde-gtk-config kcalc sddm sddm-kcm --ignore discover
+
+# Instalando e ativando o serviço de melhoria da bateria para notebooks.
+pacman -S --noconfirm tlp
+systemctl enable --now tlp
 
 # Download e instalação do yay
 sudo pacman -S git go
