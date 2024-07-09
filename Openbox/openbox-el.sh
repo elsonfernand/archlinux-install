7e2596711d5d1e76eb6cd023413a19dad6e2b993
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-09 10:49:24 -0300

    Update openbox-el.sh

--- Openbox/openbox-el.sh
+++ Openbox/openbox-el.sh
@@ -6,18 +6,7 @@
 sudo pacman -Syy
 
 # Pacotes iniciais para instalacao do Openbox, mas nem todos sao essenciais:
-sudo pacman -S --noconfirm openbox cairo-dock obconf nitrogen gedit xterm lxterminal lightdm lightdm-slick-greeter mate-polkit screen volumeicon arandr nano chromium vlc lxappearance libreoffice-still fastfetch engrampa
-
-# Instalação do servidor de audio Pipewire:
-yes | sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-
-# Instalacao de um gerenciador de arquivos. Aqui estarei usando o Thunar (e adicionando alguns pacotes para melhor configuracao):
-sudo pacman -S --noconfirm thunar libgsf ffmpegthumbnailer evince gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc tumbler poppler-glib freetype2 libgsf unzip ntfs-3g
-
-# Instalacao dos drivers da Nvidia:
-sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-# Descomente a opcao abaixo se você usa placa de vídeo da ADM.
-# pacman -S --noconfirm xf86-video-amdgpu
+sudo pacman -S --noconfirm xorg xorg-xinit openbox cairo-dock obconf lxappearance-obconf nitrogen gedit xterm lxterminal mate-polkit arandr nano chromium vlc fastfetch engrampa
 
 # Agora a gente precisa copiar o .xinirc no diretorio do X11 para o diretorio /home:
 cp /etc/X11/xinit/xinitrc .xinitrc
@@ -35,7 +24,7 @@ mkdir -p ~/.config/openbox
 cp -a /etc/xdg/openbox ~/.config/
 
 # Por padrão o Openbox não tem os diretorios usuais na /home, precisamos instalar e rodar um pacote para os diretorios aparecerem:
-sudo pacman -S xdg-user-dirs ; xdg-user-dirs-update
+yes | sudo pacman -S xdg-user-dirs ; xdg-user-dirs-update
 
 ## Instalando o Yay: ##
 # Se certificando que tem o "git" e o "go" instalados
@@ -49,14 +38,24 @@ yes | makepkg -si
 # Indo para a home
 cd
 
-# Habilitando o LightDM
-sudo systemctl enable lightdm.service
+# Instalação do servidor de audio Pipewire:
+yes | sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
 
-# Criando um arquivo de inicializacao
-cd ..
-cd ~/.config
-mkdir openbox
-cd
+# Instalacao de um gerenciador de arquivos. Aqui estarei usando o Thunar (e adicionando alguns pacotes para melhor configuracao):
+sudo pacman -S --noconfirm thunar libgsf ffmpegthumbnailer evince gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc tumbler poppler-glib freetype2 unzip ntfs-3g
+
+# Instalacao dos drivers da Nvidia:
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# Descomente a opcao abaixo se você usa placa de vídeo da ADM.
+# pacman -S --noconfirm xf86-video-amdgpu
+# Descomente as opcoes abaixo se voceexit esta usando o VirtualBox.
+# pacman -S --noconfirm virtualbox-guest-utils
+# systemctl enable vboxservice.service
+
+# Gerenciador de login:
+yes | sudo pacman -S lightdm lightdm-slick-greeter
+# Habilitando o LightDM
+sudo systemctl enable --now lightdm.service
 
 printf "\033[00;37mPronto! Agora você pode reiniciar o sistema digitando \033[01;37reboot\033[00;37m!"
 
