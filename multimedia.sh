f7cea1418927e108bf7cf8f6cce4b19b5cb578a8
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-17 12:07:40 -0300

    Add files via upload
    Pacotes que preciso para o meu trabalho como designer gráfico.

---
+++ multimedia.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Atualizar sistema
+sudo pacman -Syu
+
+# Instalacao
+sudo pacman -S kdenlive inkscake gimp ardour darktable opencl-nvidia opencl-headers libreoffice-still blender
+
+echo "Instalação dos pacotes necessários para começar a trabalhar está concluída."
+echo "Se você executou esse script antes de instalar um ambiente de trabalho, já pode reniciar o sistema."
+echo "Caso contrário pode começar a fazer dinehiro. kkkkk"
