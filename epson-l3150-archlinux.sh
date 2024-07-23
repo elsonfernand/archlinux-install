ae739a8984ed1e965e150dea0e505ce235708dd1
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-23 12:44:10 -0300

    Update epson-l3150-archlinux.sh
    Adição de alguns pacotes direto do AUR sem ajudante (helper) e reordenação de ordem de instalação.

--- epson-l3150-archlinux.sh
+++ epson-l3150-archlinux.sh
@@ -29,35 +29,60 @@ sudo systemctl enable --now ipp-usb.service
 # Utilitário para escanear documentos
 sudo pacman -S --noconfirm simple-scan
 
+# Baixar e instalar o utilitário Imagescan Plugin Networkscan
+git clone https://aur.archlinux.org/epsonscan2.git
+cd epsonscan2
+yes | makepkg -si
+cd ..
+rm -rf epsonscan2
+
+# Baixar e instalar o driver Epson do AUR
+git clone https://aur.archlinux.org/epson-inkjet-printer-escpr.git
+cd epson-inkjet-printer-escpr
+yes | makepkg -si
+cd ..
+rm -rf epson-inkjet-printer-escpr
+
 # Baixar e instalar o driver Epson do AUR
 git clone https://aur.archlinux.org/epson-inkjet-printer-escpr2.git
 cd epson-inkjet-printer-escpr2
 yes | makepkg -si
 cd ..
+rm -rf epson-inkjet-printer-escpr2
+
+# Baixar e instalar o descoberta de scanner do Epson do AUR
+git clone https://aur.archlinux.org/iscan-plugin-network.git
+cd iscan-plugin-network
+yes | makepkg -si
+cd ..
+rm -rf iscan-plugin-network
 
 # Baixar e instalar o utilitário Epson do AUR
 git clone https://aur.archlinux.org/epson-printer-utility.git
 cd epson-printer-utility
 yes | makepkg -si
 cd ..
+rm -rf epson-printer-utility
 
 # Baixar e instalar o utilitário Imagescan Plugin Networkscan
 git clone https://aur.archlinux.org/qt5-singleapplication.git
 cd qt5-singleapplication
 yes | makepkg -si
 cd ..
-
-# Baixar e instalar o utilitário Imagescan Plugin Networkscan
-git clone https://aur.archlinux.org/epsonscan2.git
-cd epsonscan2
-yes | makepkg -si
-cd ..
-
+rm -rf qt5-singleapplication
 
 # Baixar e instalar o utilitário Imagescan Plugin Networkscan
 git clone https://aur.archlinux.org/imagescan-plugin-networkscan.git
 cd imagescan-plugin-networkscan
 yes | makepkg -si
 cd ..
+rm -rf imagescan-plugin-networkscan
+
+# Baixar e instalar o utilitário Imagescan Plugin Networkscan
+git clone https://aur.archlinux.org/iscan.git
+cd iscan
+yes | makepkg -si
+cd ..
+rm -rf iscan
 
 echo "Configuração concluída. Reinicie o sistema para garantir que todas as alterações tenham efeito."
