fc3d17e6f78c10e73d44340d8459f77a4a04682a
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-24 06:55:39 -0300

    Update install-yay.sh
    Adicionado função para instalar pacote apenas se o mesmo ainda não estiver instalado.

--- install-yay.sh
+++ install-yay.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Função para verificar se um pacote está instalado
+function instalar_pacote {
+    for pacote in "$@"; do
+        if ! pacman -Qs $pacote > /dev/null ; then
+            echo "Instalando $pacote..."
+            sudo pacman -S --noconfirm $pacote
+        else
+            echo "$pacote já está instalado."
+        fi
+    done
+}
+
 # Se certificando que tem o "git", "go" e "base-devel" estao instalados
-yes | sudo pacman -S git go base-devel
+instalar_pacote git go base-devel
 # Baixando o código fonte:
 git clone https://aur.archlinux.org/yay.git
 # Acessando a pasta:
