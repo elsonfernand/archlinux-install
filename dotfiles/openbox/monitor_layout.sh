8756eca7f812cd137ae4336fbbfe205074052a5d
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-24 07:12:32 -0300

    Update monitor_layout.sh

--- dotfiles/openbox/monitor_layout.sh
+++ dotfiles/openbox/monitor_layout.sh
@@ -2,4 +2,4 @@
 
 # Configuração do xrandr para dois monitores
 # Monitor secundário à esquerda
-xrandr --output HDMI-1 --primary --auto --output HDMI-2 --auto --left-of HDMI-1
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
