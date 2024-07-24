7428cc49457eba31484b5877669cdd445eefef82
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-07-24 07:50:39 -0300

    Update and rename monitor_layout.sh to dual-monitor.sh

--- dotfiles/openbox/dual-monitor.sh
+++ dotfiles/openbox/dual-monitor.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-# Configuração do xrandr para dois monitores
-# Monitor secundário à esquerda
+# SECOND MONITOR AT LEFT
 xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
