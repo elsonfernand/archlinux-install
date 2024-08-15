dd514bed62c31fdc0a4a29ae88db5002a184be8a
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-08-15 19:50:07 -0300

    Update kde-minimal-el.sh
    Adição do navegador Brave como atual opção ativa.

--- kde-minimal-el.sh
+++ kde-minimal-el.sh
@@ -18,7 +18,23 @@ sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup ; reflector --v
 #█▄▀ █▀▄ █▀▀   █▀█ █░░ ▄▀█ █▀ █▀▄▀█ ▄▀█   █▄▄
 #█░█ █▄▀ ██▄   █▀▀ █▄▄ █▀█ ▄█ █░▀░█ █▀█   █▄█
 # Instalacao do KDE Plasma 6 sem Discover e do gerenciador de login
-sudo pacman -S --noconfirm bash-completion fastfetch plasma-desktop git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber nvidia nvidia-utils nvidia-settings dolphin dolphin-plugins ffmpegthumbs ark konsole okular gwenview kscreen chromium kate kde-gtk-config kcalc sddm sddm-kcm --ignore discover
+sudo pacman -S --noconfirm bash-completion fastfetch plasma-desktop pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber nvidia nvidia-utils nvidia-settings dolphin dolphin-plugins ffmpegthumbs ark konsole okular gwenview kscreen kate kde-gtk-config kcalc sddm sddm-kcm --ignore discover
+
+#█▄░█ ▄▀█ █░█ █▀▀ █▀▀ ▄▀█ █▀▄ █▀█ █▀█
+#█░▀█ █▀█ ▀▄▀ ██▄ █▄█ █▀█ █▄▀ █▄█ █▀▄
+#Desconente a opção que te agradar ou use o Brave
+#Chromium, navegador de código aberto (tipo o Google Chrome mas sem o dedo do Google)
+#sudo pacman -S --noconfirm chromium
+#Firefox, navegador altamente difundido nas mais variadas distribuições
+#sudo pacman -S --noconfirm firefox
+#█▄▄ █▀█ ▄▀█ █░█ █▀▀   █▄▄ █▀█ █▀█ █░█░█ █▀ █▀▀ █▀█
+#█▄█ █▀▄ █▀█ ▀▄▀ ██▄   █▄█ █▀▄ █▄█ ▀▄▀▄▀ ▄█ ██▄ █▀▄
+# Caso queira instalar o Brave (navegador de código aberto, com bloqueador de anúncios embutido e baseado no Chromium) sem ajudante do AUR #
+git clone https://aur.archlinux.org/brave-bin.git
+cd brave-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf brave-bin
 
 #▀█▀ █░░ █▀█
 #░█░ █▄▄ █▀▀
