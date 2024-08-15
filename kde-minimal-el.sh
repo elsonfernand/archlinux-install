e7a7fb042c4548f6aacd7a73398edec9a9959772
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-08-15 19:39:43 -0300

    Update kde-minimal-el.sh
    Adição de texto em formato ASCII separando sessões e retirada de redundâncias de outros scripts.

--- kde-minimal-el.sh
+++ kde-minimal-el.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#█▀█ █▀▀ █░░ █▀█ █▀▀ █ █▀█
+#█▀▄ ██▄ █▄▄ █▄█ █▄█ █ █▄█
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-
-# Reflector
+#█▀█ █▀▀ █▀▀ █░░ █▀▀ █▀▀ ▀█▀ █▀█ █▀█
+#█▀▄ ██▄ █▀░ █▄▄ ██▄ █▄▄ ░█░ █▄█ █▀▄
 # Instalando
 sudo pacman -S --noconfirm reflector
 # Ativando
@@ -12,34 +14,19 @@ sudo systemctl enable reflector.timer
 # Configurando
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup ; reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
+
+#█▄▀ █▀▄ █▀▀   █▀█ █░░ ▄▀█ █▀ █▀▄▀█ ▄▀█   █▄▄
+#█░█ █▄▀ ██▄   █▀▀ █▄▄ █▀█ ▄█ █░▀░█ █▀█   █▄█
 # Instalacao do KDE Plasma 6 sem Discover e do gerenciador de login
 sudo pacman -S --noconfirm bash-completion fastfetch plasma-desktop git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber nvidia nvidia-utils nvidia-settings dolphin dolphin-plugins ffmpegthumbs ark konsole okular gwenview kscreen chromium kate kde-gtk-config kcalc sddm sddm-kcm --ignore discover
 
+#▀█▀ █░░ █▀█
+#░█░ █▄▄ █▀▀
 # Instalando e ativando o servico de melhoria da bateria para notebooks.
 sudo pacman -S --noconfirm tlp
 sudo systemctl enable --now tlp
 
-# Pacotes e ativação da impressora Epson L3150
-# sudo pacman -S --noconfirm cups ghostscript gsfonts libcups gutenprint foomatic-db-gutenprint-ppds
-# sudo systemctl enable --now cups.service ; sudo systemctl enable --now cups.socket ; sudo systemctl enable --now cups.path
-# sudo pacman -S --noconfirm system-config-printer
-# sudo usermod -aG lp $USER
-# sudo pacman -S sane-airscan sane ipp-usb
-# sudo usermod -aG saned,scanner $USER
-# sudo systemctl enable --now ipp-usb.service
-# sudo pacman -S simple-scan
-# yay -S epson-inkjet-printer-escpr epsonscan2 imagescan-plugin-networkscan
-
-# Download e instalacao do Yay
-sudo pacman -S git go
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-# Instalando e ativando a SWAP. Arquivo editavel em /etc/default/zramd
-cd ..
-yay -S --noconfirm zramd
-sudo systemctl enable --now zramd.service
-
+#█▀ █▀▄ █▀▄ █▀▄▀█
+#▄█ █▄▀ █▄▀ █░▀░█
 # Ativando o gerenciador de login SDDM
 sudo systemctl enable --now sddm
