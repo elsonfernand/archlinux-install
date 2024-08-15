bf0996f4c0b29fee12ccae44a95457f392f6e21b
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-08-15 20:19:55 -0300

    Update multimedia.sh
    Adição de texto ASCII para separar sessões.

--- multimedia.sh
+++ multimedia.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
+#╭━━━╮╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╭╮╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱
+#┃╭━╮┃╱╱╱╱╱╱╱╭╯╰╮╱╱╱╱╱╱╱╱╱╱╱╱╱┃┣╯╰╮╱╱╱╱╱╱╱╱┃┃╱╱╱╱
+#┃╰━╯┣━━┳━━┳━┻╮╭╋━━┳━━╮╱╭╮╭┳╮╭┫┣╮╭╋┳╮╭┳━━┳━╯┣┳━━╮
+#┃╭━━┫╭╮┃╭━┫╭╮┃┃┃┃━┫━━┫╱┃╰╯┃┃┃┃┃┃┃┣┫╰╯┃┃━┫╭╮┣┫╭╮┃
+#┃┃╱╱┃╭╮┃╰━┫╰╯┃╰┫┃━╋━━┃╱┃┃┃┃╰╯┃╰┫╰┫┃┃┃┃┃━┫╰╯┃┃╭╮┃
+#╰╯╱╱╰╯╰┻━━┻━━┻━┻━━┻━━╯╱╰┻┻┻━━┻━┻━┻┻┻┻┻━━┻━━┻┻╯╰╯
+
 # Atualizacao do sistema
 sudo pacman -Syu
 
+#█▄▀ █▀▄ █▀▀ █▄░█ █░░ █ █░█ █▀▀
+#█░█ █▄▀ ██▄ █░▀█ █▄▄ █ ▀▄▀ ██▄
 #Para que o Kdenlive tenha uma interface bonitinha tem que ser instalado o tema "breeze"
-sudo pacman -S --noconfirm breeze
+sudo pacman -S --noconfirm breeze kdenlive
 
+#█▀▄ █▀▀ █▀ █ █▀▀ █▄░█  █▀▀ █▀█ ▄▀█ █▀▀ █ █▀▀ █▀█
+#█▄▀ ██▄ ▄█ █ █▄█ █░▀█  █▄█ █▀▄ █▀█ █▀░ █ █▄▄ █▄█
 # Instalacao de pacotes design gráfico. Exclua os tres ultimos pacotes se nao for lidar com imagens RAW.
-sudo pacman -S --noconfirm kdenlive inkscape scribus gimp blender darktable opencl-nvidia opencl-headers font-manager
+sudo pacman -S --noconfirm inkscape scribus gimp blender darktable opencl-nvidia opencl-headers font-manager
 
+#▄▀█ █░█ █▀▄ █ █▀█
+#█▀█ █▄█ █▄▀ █ █▄█
 # Instalacao audio: gravacao de guitarra e sugestoes de plugins (Ardour para gravacao, o Guitarix apenas para fazer um som e o resto sao plugins)
 sudo pacman -S --noconfirm ardour guitarix gxplugins.lv2 lv2 carla x42-plugins avldrums.lv2 lsp-plugins-lv2 calf noise-repellent dragonfly-reverb-lv2 master_me-lv2 eq10q gmsynth.lv2 zam-plugins-lv2 
 
+#█ █▀█   █░░ █▀█ ▄▀█ █▀▄ █▀▀ █▀█
+#█ █▀▄   █▄▄ █▄█ █▀█ █▄▀ ██▄ █▀▄
 # Plugin carregador de IR e mixer de modelo neural para Linux/Windows.
 git clone https://github.com/brummer10//Ratatouille.lv2.git
 cd Ratatouille.lv2
 git submodule update --init --recursive
 make
-make install # will install into ~/.lv2 ... AND/OR....
+# make install # will install into ~/.lv2 ... AND/OR....
 sudo make install # will install into /usr/lib/lv2
 
 # Caso já esteja com uma interface grafica instalada recomendo o pacote de IR gratuito GuitarHack Impulses que se encontra no link para copiar e colar no navegador de sua preferencia: https://my.pcloud.com/publink/show?code=XZIK5QkZMpbl49KMeHQy3dmnzMTPojdJSReX
 
-# Instalacao de editor de texto
+#█▀▀ █▀▄ █ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   ▀█▀ █▀▀ ▀▄▀ ▀█▀ █▀█
+#██▄ █▄▀ █ ░█░ █▄█ █▀▄   █▄▀ ██▄   ░█░ ██▄ █░█ ░█░ █▄█
 sudo pacman -S --noconfirm libreoffice-still
 
 echo "Instalação dos pacotes necessários para começar a trabalhar está concluída."
