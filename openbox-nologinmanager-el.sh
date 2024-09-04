681e9d3ce9de35dac3942796f1412c3a67963a15
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-09-04 11:30:16 -0300

    Update openbox-ly-login-manager-el.sh
    Adição e ativação do ly (login manager) e do Dunst (notification-daemon).

--- openbox-nologinmanager-el.sh
+++ openbox-nologinmanager-el.sh
@@ -31,10 +31,14 @@ sudo pacman -S --noconfirm lxappearance-obconf
 sudo pacman -S --noconfirm tint2 network-manager-applet xfce4-power-manager gsimplecal 
 
 # Instala utilitários adicionais e gerenciador de arquivos #
-sudo pacman -S --noconfirm thunar thunar-archive-plugin volumeicon alsa-utils alsa-plugins gpicview gvfs gvfs-mtp gvfs-smb gvfs-gphoto2 gvfs-afc menumaker tumbler ffmpegthumbnailer unzip p7zip xarchiver vlc archlinux-wallpaper fastfetch picom libxml2 zathura zathura-pdf-mupdf geany geany-plugins telegram-desktop i3lock playerctl brightnessctl
+sudo pacman -S --noconfirm thunar thunar-archive-plugin dunst volumeicon alsa-utils alsa-plugins gpicview gvfs gvfs-mtp gvfs-smb gvfs-gphoto2 gvfs-afc menumaker tumbler ffmpegthumbnailer poppler-glib libgsf libgepub libopenraw freetype2 unzip p7zip xarchiver vlc archlinux-wallpaper fastfetch picom libxml2 zathura zathura-pdf-mupdf geany geany-plugins telegram-desktop i3lock playerctl brightnessctl
 
 # Cliente de email
 sudo pacman -S --noconfirm thunderbird systray-x-common thunderbird-i18n-pt-br thunderbird-i18n-en-us
+
+# Login Manager (Nerd Style)
+sudo pacman -S ly
+sudo systemctl enable --now ly.service
 
 # Instala navegador. Descomente a linha da sua preferencia. Eu dou preferencia a navegador baseado no Chromium. #
 #sudo pacman -S --noconfirm chromium
