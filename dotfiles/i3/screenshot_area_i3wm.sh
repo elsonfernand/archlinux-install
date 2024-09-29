7676850b8f7a9e48ca6f61b4d908ac1f327dd203
Elson Fernandes <153093967+elsonfernand@users.noreply.github.com>
2024-09-29 12:47:24 -0300

    Add files via upload

---
+++ dotfiles/i3/screenshot_area_i3wm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define o diretório e o nome base para as capturas de tela
+DIR=~/Pictures/Screenshots
+BASE_NAME="Screenshot-$(date +'%Y-%m-%d')"
+
+# Cria o diretório se não existir
+mkdir -p "$DIR"
+
+# Incrementa o nome do arquivo se ele já existir
+FILE_NAME="$BASE_NAME.png"
+COUNT=1
+while [ -e "$DIR/$FILE_NAME" ]; do
+    FILE_NAME="${BASE_NAME}_#$(printf "%03d" $COUNT).png"
+    COUNT=$((COUNT + 1))
+done
+
+# Executa o comando scrot com a opção -s
+/usr/bin/scrot -s "$DIR/$FILE_NAME"
